c4741f1530b6a0c1f33e57146e0991349e5015be
MuhammadM1998 <MuhammadM.1097@gmail.com>
2022-11-27 17:47:01 +0200

    Added Header and Footer

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,1 +1,121 @@
-export default defineNuxtConfig({});
+export default defineNuxtConfig({
+  app: {
+    head: {
+      title: 'Arab Sat Sharing - سات شيرنج العرب',
+      htmlAttrs: { lang: 'ar' },
+      link: [
+        // Favicon
+        {
+          rel: 'icon',
+          href: '/favicon-32x32.png',
+          type: 'image/png',
+          sizes: '32x32',
+        },
+        {
+          rel: 'apple-touch-icon',
+          href: '/favicon-16x16.png',
+          sizes: '16x16',
+        },
+        {
+          rel: 'apple-touch-icon',
+          href: '/apple-touch-icon.png',
+          sizes: '180x180',
+        },
+        {
+          rel: 'mask-icon',
+          href: '/safari-pinned-tab.svg',
+          color: '#5bbad5',
+        },
+        {
+          rel: 'manifest',
+          href: '/site.webmanifest',
+        },
+        // Fonts
+        {
+          rel: 'preconnect',
+          href: 'https://fonts.bunny.net',
+        },
+        {
+          rel: 'stylesheet',
+          href: 'https://fonts.bunny.net/css?family=tajawal:400,500,700,800',
+        },
+      ],
+
+      meta: [
+        { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+        // Favicon
+        { name: 'msapplication-TileColor', content: '#00aba9' },
+        { name: 'theme-color', content: '#ffffff' },
+        // Social Cards Meta
+        { property: 'fb:app_id', content: '966242223397117' },
+        { property: 'og:type', content: 'website' },
+        { name: 'twitter:card', content: 'summary_large_image' },
+        {
+          property: 'og:url',
+          content: 'https://arab-sat-sharing.vercel.app/',
+        },
+        {
+          property: 'twitter:url',
+          content: 'https://arab-sat-sharing.vercel.app/',
+        },
+        {
+          property: 'twitter:domain',
+          content: 'https://arab-sat-sharing.vercel.app/',
+        },
+        {
+          property: 'og:image',
+          content:
+            'https://res.cloudinary.com/cloud-m98/image/upload/v1663009658/Portfolio/Website-Screenshot.png',
+        },
+        {
+          property: 'og:image:secure_url',
+          content:
+            'https://res.cloudinary.com/cloud-m98/image/upload/v1663009658/Portfolio/Website-Screenshot.png',
+        },
+        {
+          property: 'twitter:image',
+          content:
+            'https://res.cloudinary.com/cloud-m98/image/upload/v1663009658/Portfolio/Website-Screenshot.png',
+        },
+        { property: 'og:image:width', content: '1200' },
+        { property: 'og:image:height', content: '630' },
+        {
+          name: 'description',
+          content:
+            'جدول مباريات اليوم مع قنوات الشيرنج الناقلة علي جميع الأقمار',
+        },
+        {
+          property: 'og:description',
+          content:
+            'جدول مباريات اليوم مع قنوات الشيرنج الناقلة علي جميع الأقمار',
+        },
+        {
+          property: 'twitter:description',
+          content:
+            'جدول مباريات اليوم مع قنوات الشيرنج الناقلة علي جميع الأقمار',
+        },
+
+        {
+          property: 'og:title',
+          content: 'Arab Sat Sharing - سات شيرنج العرب',
+        },
+        {
+          property: 'og:site_name',
+          content: 'Arab Sat Sharing - سات شيرنج العرب',
+        },
+        {
+          property: 'twitter:title',
+          content: 'Arab Sat Sharing - سات شيرنج العرب',
+        },
+        {
+          property: 'og:image:alt',
+          content: 'Arab Sat Sharing - سات شيرنج العرب',
+        },
+      ],
+    },
+  },
+
+  modules: ['@nuxtjs/tailwindcss', 'nuxt-icon', '@nuxtjs/color-mode'],
+
+  colorMode: { classSuffix: '' },
+});
--- pages/index.vue
+++ pages/index.vue
@@ -1,10 +1,3 @@
-<script setup>
-  const { data } = await useFetch('/fixtures');
-</script>
-
 <template>
-  <div>
-    <p>Nuxt with Playwright Demo</p>
-    <p>{{ data }}</p>
-  </div>
+  <main class="grow">Placeholder Text</main>
 </template>
