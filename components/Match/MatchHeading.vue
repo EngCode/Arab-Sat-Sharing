cdb28f56f7de8173e13fe4966c5a2dcb899f823f
MuhammadM1998 <MuhammadM.1097@gmail.com>
2022-11-29 21:33:13 +0200

    Fixed grid bug

--- components/Match/MatchHeading.vue
+++ components/Match/MatchHeading.vue
@@ -25,30 +25,30 @@
 
 <style scoped lang="scss">
   .match-heading {
-    @apply my-2 grid items-center gap-x-3 gap-y-1.5;
-
-    grid-template-areas:
-      'home home time'
-      'away away time';
-
-    @media (min-width: 480px) {
-      grid-template-areas: 'home home home home home home time away away away away away away';
-    }
+    @apply my-2 items-center;
+    @apply grid  gap-x-3 gap-y-1.5;
+    @apply grid-cols-3 grid-rows-2;
+    @apply 2xs:grid-cols-7 2xs:grid-rows-1;
   }
 
   .team {
-    @apply flex items-center justify-between gap-2 2xs:justify-end;
+    @apply flex items-center justify-between gap-2;
+    @apply col-span-2  row-span-1;
+    @apply 2xs:col-span-3 2xs:justify-end;
 
     &--home {
-      @apply [grid-area:home];
+      @apply row-start-1;
     }
 
     &--away {
-      @apply [grid-area:away] 2xs:flex-row-reverse;
+      @apply row-start-2;
+      @apply 2xs:row-start-1 2xs:flex-row-reverse;
     }
   }
 
   .time {
-    @apply text-center font-semibold text-gray-300 [grid-area:time] dark:text-blue-100;
+    @apply text-center font-semibold text-gray-300  dark:text-blue-100;
+    @apply col-span-1 col-start-3 row-span-2 row-start-1;
+    @apply 2xs:col-start-4 2xs:-mt-1.5;
   }
 </style>
