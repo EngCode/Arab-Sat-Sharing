ae901f73ea2c736eba65a25608235496e712e942
MuhammadM1998 <MuhammadM.1097@gmail.com>
2022-12-04 06:36:37 +0200

    Refactored fonts for better perf

--- config/link/fonts.js
+++ config/link/fonts.js
@@ -6,6 +6,8 @@ const FONTS = [
   {
     rel: 'stylesheet',
     href: 'https://fonts.bunny.net/css?family=tajawal:400,500,700,800&display=swap',
+    media: 'print',
+    onload: "this.media='all'",
   },
 ];
 
