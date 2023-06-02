0dc9cff19baf42a2f412efff8877299076e0394f
Muhammad Mahmoud <MuhammadM.1097@gmail.com>
2023-06-02 15:15:41 +0300

    feat: add no matches found section

--- components/CompetitionsWrapper.vue
+++ components/CompetitionsWrapper.vue
@@ -12,12 +12,28 @@
     />
 
     <p class="text-lg font-semibold">
-      حصل خطأ اثناء الحصول علي البيانات. نأسف لذلك وسنحاول إصلاح الخطأ في أسرع
+      حدث خطأ اثناء الحصول علي البيانات. نأسف لذلك وسنحاول إصلاح الخطأ في أسرع
       وقت!
     </p>
   </section>
 
-  <section v-else-if="data" class="flex flex-col gap-5 py-4">
+  <section
+    v-else-if="data.competitions.length === 0"
+    class="text-center [direction:rtl]"
+  >
+    <BaseImg
+      src="No_Matches"
+      class="mx-auto mb-8"
+      height="384"
+      alt="Player holding a football in his hand"
+    />
+
+    <p class="text-lg font-semibold">
+      لا توجد مباريات اليوم. حاول العودة لاحقاً.
+    </p>
+  </section>
+
+  <section v-else class="flex flex-col gap-5 py-4">
     <CompetitionCard
       v-for="(competition, index) in data.competitions"
       :key="index"
